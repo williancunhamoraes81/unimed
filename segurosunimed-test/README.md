274bef54720dd50a1d39bb05d98faa2d6fd5aa60
FIlipe Mauler <mauler.negocios@gmail.com>
2024-01-04 21:16:09 -0300

    Delete segurosunimed-test directory

--- segurosunimed-test/README.md
+++
@@ -1,1 +0,0 @@
-# segurosunimed-test
--- segurosunimed-test/target/classes/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO customer (id, name, email) VALUES (1, 'Homem Aranha', 'aranha@vingadores.com');
-INSERT INTO customer (id, name, email) VALUES (2, 'Thor', 'thor@vingadores.com');
-INSERT INTO customer (id, name, email) VALUES (3, 'Homem de Ferro', 'homemdeferro@vingadores.com');
-INSERT INTO customer (id, name, email) VALUES (4, 'Namor', 'namor@vingadores.com');
-INSERT INTO customer (id, name, email) VALUES (5, 'Hulk', 'hulk@vingadores.com');
